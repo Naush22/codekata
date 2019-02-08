6b6cc5e082d89a9995c291923692fc3b64d76665
Naush22 <47441449+Naush22@users.noreply.github.com>
2019-02-08 14:59:29 +0530

    Rename program4.py to programM.py

---
+++ programM.py
@@ -0,0 +1,7 @@
+m=(raw_input())
+if m in ('a','e','i','o','u'):
+    print("Vowel")
+elif m in ('b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'):
+    print("Consonant")
+else:
+    print("invalid")
